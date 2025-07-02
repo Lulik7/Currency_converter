eba165886fb5d7320c43bf510b49480ceeed7db3
Lulik B <lulikartik7@gmail.com>
2025-07-02 21:30:37 +0300

    Update country-list.js
    upd

---
+++ country-list.js
@@ -0,0 +1,31 @@
+const countryList = {
+    "USD": "US", // United States
+    "NPR": "NP", // Nepal
+    "INR": "IN", // India
+    "PKR": "PK", // Pakistan
+    "MAD": "MA", // Morocco
+    "EUR": "EU", // Eurozone
+    "GBP": "GB", // United Kingdom
+    "JPY": "JP", // Japan
+    "AUD": "AU", // Australia
+    "CAD": "CA", // Canada
+    "RUB": "RU", // Russia
+    "UAH": "UA", // Ukraine
+    "KZT": "KZ", // Kazakhstan
+    "BYN": "BY", // Belarus
+    "PLN": "PL", // Poland
+    "CNY": "CN", // China
+    "BRL": "BR", // Brazil
+    "MXN": "MX", // Mexico
+    "CHF": "CH", // Switzerland
+    "SEK": "SE", // Sweden
+    "NOK": "NO", // Norway
+    "DKK": "DK", // Denmark
+    "NZD": "NZ", // New Zealand
+    "SGD": "SG", // Singapore
+    "HKD": "HK", // Hong Kong
+    "ZAR": "ZA", // South Africa
+    "EGP": "EG", // Egypt
+    "ILS": "IL", // Israel (Israeli New Shekel)
+
+};
